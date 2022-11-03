ccfe382e91d6829f0f2a5e0dee5974f0fa526282
Thunderbird93 <antircristian@gmail.com>
2022-11-03 15:48:00 +0100

    [add] Vue

---
+++ js/app.js
@@ -0,0 +1,10 @@
+/*
+- Inserire le immagini nel thumb -
+- Mostrare in grande l'immagine della thumb -
+- aggiungere la class active all'immagine attiva -
+*/
+const { createApp } = Vue;
+
+createApp({
+
+}).mount('#app')
